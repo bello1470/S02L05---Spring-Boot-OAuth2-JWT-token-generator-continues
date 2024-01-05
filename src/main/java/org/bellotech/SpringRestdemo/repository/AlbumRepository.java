d385fe6e6217213212d7daca5a2b2bab4d6ba05d
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-05 18:48:43 +0000

    repo commits

--- src/main/java/org/bellotech/SpringRestdemo/repository/AlbumRepository.java
+++ src/main/java/org/bellotech/SpringRestdemo/repository/AlbumRepository.java
@@ -1,10 +1,14 @@
 package org.bellotech.SpringRestdemo.repository;
 
+import java.util.List;
+
 import org.bellotech.SpringRestdemo.model.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long>{
+
+    List<Album> findByAccount_id(long id);
     
 }
