ffd2804bc6e306061b41e7ab8a3fee2d224fac6e
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-05 10:06:49 +0000

    add join Album commits

--- src/main/java/org/bellotech/SpringRestdemo/model/Album.java
+++ src/main/java/org/bellotech/SpringRestdemo/model/Album.java
@@ -1,17 +1,26 @@
 package org.bellotech.SpringRestdemo.model;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Album {
 
     @Id
@@ -21,8 +30,8 @@ public class Album {
 
     private String name;
 
-    @Column
-    @JoinColumn(name = "Album_id", referencedColumnName = "id")
+    @ManyToOne
+    @JoinColumn(name = "Account_id", referencedColumnName = "id", nullable = false)
     private Account account;
     
 }
