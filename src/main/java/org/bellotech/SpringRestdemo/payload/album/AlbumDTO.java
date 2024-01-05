bb81894d0f31ec70388fb96acccd8fb280609766
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-05 10:08:04 +0000

    album dto refa. commit

---
+++ src/main/java/org/bellotech/SpringRestdemo/payload/album/AlbumDTO.java
@@ -0,0 +1,28 @@
+package org.bellotech.SpringRestdemo.payload.album;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlbumDTO {
+
+    @NotBlank
+    @Schema(description = "Album name", example = "Travel", requiredMode = RequiredMode.REQUIRED)
+    private String description;
+
+
+    @NotBlank
+    @Schema(description = "Description of the name", example = "Description", requiredMode = RequiredMode.REQUIRED)
+    private String name;
+    
+}
