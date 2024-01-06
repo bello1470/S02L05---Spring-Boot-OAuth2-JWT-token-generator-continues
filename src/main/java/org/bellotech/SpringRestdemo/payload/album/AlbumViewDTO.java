e874aa386793d9398e5b20943730cffdc1ebb130
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-06 13:30:40 +0000

    Refactor commits

--- src/main/java/org/bellotech/SpringRestdemo/payload/album/AlbumViewDTO.java
+++ src/main/java/org/bellotech/SpringRestdemo/payload/album/AlbumViewDTO.java
@@ -21,12 +21,12 @@ public class AlbumViewDTO {
 private long id;
 
 
-    @NotBlank
+   
     @Schema(description = "Album name", example = "Travel", requiredMode = RequiredMode.REQUIRED)
     private String name;
 
 
-    @NotBlank
+   
     @Schema(description = "Description of the name", example = "Description", requiredMode = RequiredMode.REQUIRED)
     private String description;
     
