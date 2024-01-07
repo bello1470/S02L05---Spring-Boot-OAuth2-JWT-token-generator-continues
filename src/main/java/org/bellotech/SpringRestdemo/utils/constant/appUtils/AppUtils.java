564bf781fe51e85bda19bff5713bb9b960cad167
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-07 12:23:25 +0000

    thumbnail config commits

--- src/main/java/org/bellotech/SpringRestdemo/utils/constant/appUtils/AppUtils.java
+++ src/main/java/org/bellotech/SpringRestdemo/utils/constant/appUtils/AppUtils.java
@@ -5,15 +5,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import javax.imageio.ImageIO;
+
+import org.imgscalr.Scalr;
+import java.awt.image.BufferedImage;
+import org.springframework.web.multipart.MultipartFile;
+import org.imgscalr.Scalr;
 public class AppUtils {
 
      // we created String name, receiving filename 
      
-    public static String get_photo_upload_path(String fileNmae, long album_id) throws IOException{
+    public static String get_photo_upload_path(String fileName, String folder_name, long album_id) throws IOException{
+        String path = "src\\main\\resources\\static\\uploads\\"+album_id+"\\"+folder_name;
+        Files.createDirectories(Paths.get(path));
+        return new File(path).getAbsolutePath() + "\\" + fileName;
 
-       
-        Files.createDirectories(Paths.get("src\\main\\resources\\static\\uploads\\" + album_id));
-        return new File("src\\main\\resources\\static\\uploads\\" + album_id).getAbsolutePath() + "\\" + fileNmae;
     }
+
+    public static BufferedImage getThumbnail(MultipartFile orginalFile, Integer width) throws IOException{  
+        BufferedImage thumbImg = null;  
+        BufferedImage img = ImageIO.read(orginalFile.getInputStream());  
+        thumbImg = Scalr.resize(img, Scalr.Method.AUTOMATIC, Scalr.Mode.AUTOMATIC, width, Scalr.OP_ANTIALIAS);   
+        return thumbImg;  
+    }
+    
 }
 
