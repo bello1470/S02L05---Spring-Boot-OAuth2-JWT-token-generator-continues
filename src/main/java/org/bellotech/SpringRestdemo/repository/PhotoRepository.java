629d24e7c1ff4a280a1cdecf02bf520b1c04f51f
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-07 10:17:26 +0000

    photorepo commit

--- src/main/java/org/bellotech/SpringRestdemo/repository/PhotoRepository.java
+++ src/main/java/org/bellotech/SpringRestdemo/repository/PhotoRepository.java
@@ -2,14 +2,14 @@ package org.bellotech.SpringRestdemo.repository;
 
 import java.util.List;
 
-
 import org.bellotech.SpringRestdemo.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long>{
 
-    List<Photo> findByAccount_id(long id);
+    //List<Photo> findByAccount_id(long id);
     
 }
