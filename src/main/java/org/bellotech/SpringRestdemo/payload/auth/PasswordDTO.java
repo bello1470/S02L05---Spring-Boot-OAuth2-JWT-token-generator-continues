be61eab6019faf90b1979e98f3e9e9fd16f3fe32
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-01-08 05:29:40 +0000

    passworddto commits

--- src/main/java/org/bellotech/SpringRestdemo/payload/auth/PasswordDTO.java
+++ src/main/java/org/bellotech/SpringRestdemo/payload/auth/PasswordDTO.java
@@ -2,11 +2,8 @@ package org.bellotech.SpringRestdemo.payload.auth;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-
 import lombok.Getter;
-
 import lombok.Setter;
 
 @Setter
